52f25e5b914028d0ccd3e7705393e9a553884a9d
matheusldev <mgdesenvolvedor@gmail.com>
2025-01-05 22:09:38 -0300

    feat: criando tela inicial para listar os produtos com funcionalidade de adição, edição e deleção de produtos.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import { useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import { Product } from '@/types/types';
+import { useQuery } from '@tanstack/react-query';
+import Logo from '@/assets/images/logo-xsolution.png';
+import Image from 'next/image';
+import './globals.css';
+import './page.css';
+import ProductModal from '@/components/ModalProduct';
+import { deleteProduct, fetchProducts } from '@/services/productsService';
+import { formatValue } from '@/utils/currency';
+
+export default function Page() {
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const { data: produtos, isLoading } = useQuery<Product[]>({
+    queryKey: ['produtos'],
+    queryFn: fetchProducts,
+  });
+
+  const handleEdit = (product: Product) => {
+    // Lógica de edição
+    console.log('Editar produto', product);
+    setDialogVisible(true);
+  };
+
+  const priceBodyTemplate = (rowData: Product) => {
+    return formatValue(rowData.price);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <header>
+        <Image 
+          src={Logo} 
+          alt="Logo X-Solutions" 
+          className="image" 
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      </header>
+      <main>
+        <div className='sub-header'>
+          <h1>Produtos cadastrados</h1>
+          <Button label="Criar Produto" icon="pi pi-plus" onClick={() => setDialogVisible(true)} className='space'/>
         </div>
+        <DataTable value={produtos} className="centered-table">
+          <Column field="id" header="ID" />
+          <Column field="name" header="Nome" />
+          <Column 
+            field="price" 
+            header="Preço" 
+            body={priceBodyTemplate} 
+          />
+          <Column field="quantity" header="Quantidade" />
+          <Column 
+            header="Ações" 
+            body={(rowData: Product) => (
+              <div className="actions">
+                <Button 
+                  icon="pi pi-pencil" 
+                  className="p-button-rounded p-button-info p-button-text" 
+                  onClick={() => handleEdit(rowData)} 
+                />
+                <Button 
+                  icon="pi pi-trash" 
+                  className="p-button-rounded p-button-danger p-button-text" 
+                  onClick={() => deleteProduct(rowData.id)} 
+                />
+              </div>
+            )}
+          />
+        </DataTable>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      
+      <ProductModal visible={dialogVisible} onHide={() => setDialogVisible(false)} />
+    </>
   );
 }
